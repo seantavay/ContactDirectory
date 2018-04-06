1d821d2fcd3c4505d82013910323fd550a4f9bba
seantavay <u788730@dow.com>
2018-04-06 12:32:00 -0500

    Add project files.

---
+++ ContactDirectory.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ContactDirectory.API.Startup))]
+
+namespace ContactDirectory.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
